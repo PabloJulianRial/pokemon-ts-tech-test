96d9a116dc30b086d31e24c5993efe88b0a25906
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-03-13 10:44:34 +0000

    all pokemon cards are showing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import "./styles/style.scss";
+import pokemonArray from "./data/pokemon";
+
+const cardContainer = document.querySelector<HTMLElement>(".card-container")
+
+if(!cardContainer){
+    throw new Error ("Card container not found")
+}
+
+
+pokemonArray.forEach((pokemon) =>{
+    cardContainer.innerHTML += `<div class="card">
+    <div class="card__heading">${pokemon.name}</div>
+    <div class="card__content"><img src=${pokemon.sprite} alt="${pokemon.name} image" class="card__image">
+    <div class="card__text">${pokemon.types.join(" ")}</div></div>
+    
+  </div>`
+})
+
+
+
+
+
