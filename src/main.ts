8084fd56115c03bb1779e593f54ff04c703a9073
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-03-13 12:03:41 +0000

    basic test complete

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
 import "./styles/style.scss";
 import pokemonArray from "./data/pokemon";
 
-const cardContainer = document.querySelector<HTMLElement>(".card-container")
+const cardContainer = document.querySelector<HTMLElement>(".card-container");
 
-if(!cardContainer){
-    throw new Error ("Card container not found")
+if (!cardContainer) {
+  throw new Error("Card container not found");
 }
 
-
-pokemonArray.forEach((pokemon) =>{
-    cardContainer.innerHTML += `<div class="card">
-    <div class="card__heading">${pokemon.name}</div>
-    <div class="card__content"><img src=${pokemon.sprite} alt="${pokemon.name} image" class="card__image">
-    <div class="card__text">${pokemon.types.join(" ")}</div></div>
+pokemonArray.forEach((pokemon) => {
+  cardContainer.innerHTML += `<div class="card">
+    <div class="card__content">
+    <img src=${pokemon.sprite} alt="${pokemon.name} image" class="card__image">
+  <div class="card__heading" style="text-transform: capitalize">${
+    pokemon.name
+  }</div>
+    <div class="card__text style="text-align:left">${pokemon.name} (#${
+    pokemon.id
+  }) is a ${pokemon.types.join(" & ")}pokemon.</div></div>
     
-  </div>`
-})
-
-
-
-
-
+  </div>`;
+});
